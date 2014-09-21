e01de6aa5b26d67804ef2e455800f5c3d9a4a5f2
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2014-09-21 19:16:29 -0300

    Comit inicial

---
+++ Projeto/Model/Terceirizado.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto.Model
+{
+    public class Terceirizado : Funcionario
+    {
+        public string Categoria { get; set; }
+    }
+}
---
+++ Projeto/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Projeto.Model;
+
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dados do Funcionario
+            Funcionario func = new Funcionario();
+
+            func.IdFuncionario = 1;
+            func.Nome = "Glaucia Lemos";
+            func.Salario = 4000;
+
+            Console.WriteLine("----------- Dados do Funcionário -------------- ");
+            Console.WriteLine("Id do Funcionário..........: " + func.IdFuncionario);
+            Console.WriteLine("Nome do Funcionário........: " + func.Nome);
+            Console.WriteLine("Salário....................: R$ " + func.Salario);
+
+            #endregion
+
+            #region Dados do Estagiário
+            Estagiario est = new Estagiario();
+            est.IdFuncionario = 2;
+            est.Nome = "Luciano Galdino";
+            est.Salario = 1500;
+            est.Tipo = "Desenvolvedor";
+
+            Console.WriteLine("\n----------- Dados do Estagiário -------------- ");
+            Console.WriteLine("Id do Estagiário........: " + est.IdFuncionario);
+            Console.WriteLine("Nome do Estagiário......: " + est.Nome);
+            Console.WriteLine("Salário.................: R$ " + est.Salario);
+            Console.WriteLine("Tipo....................: " + est.Tipo);
+
+            #endregion
+
+            #region Dados do Terceirizado
+            Terceirizado terc = new Terceirizado();
+            terc.IdFuncionario = 3;
+            terc.Nome = "Jake Lemos";
+            terc.Salario = 3500;
+            terc.Categoria = "Suporte";
+
+            Console.WriteLine("\n----------- Dados do Terceirizado -------------- ");
+            Console.WriteLine("Id do Terceirizado.............: " + terc.IdFuncionario);
+            Console.WriteLine("Nome...........................: " + terc.Nome);
+            Console.WriteLine("Salário........................: " + terc.Salario);
+            Console.WriteLine("Categoria......................: " + terc.Categoria);
+
+            #endregion
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
